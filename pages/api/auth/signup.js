fa923007cd4739af5b595844c1f67e2c190c4109
Prajwal Adhikari <prajwalad101@gmail.com>
2022-03-07 16:49:55 +0545

    add handler function for user signup

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,37 @@
+import { hashPassword } from "../../../lib/auth";
+import { connectDb } from "../../../lib/db";
+
+async function handler(req, res) {
+  if (req.method !== "POST") {
+    return;
+  }
+  const data = req.body;
+
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({
+      message: "Invalid input - Check your fields again ",
+    });
+  }
+
+  const client = await connectDb();
+
+  const db = client.db();
+
+  const hashedPassword = hashPassword(password);
+
+  const result = db.collection("users").insertOne({
+    email: email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({ message: "Created user!" });
+}
+
+export default handler;
